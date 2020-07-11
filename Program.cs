a1d1163a54e0976fb25692f43af916a04468ab45
DJMarcos <62577982+DJMarcos@users.noreply.github.com>
2020-07-10 17:45:16 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace myconsoleProject
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.ReadLine();
+            Console.WriteLine("Remember, we're 'accounting' on you!");
+            Console.Read();
+        }
+    }
+}
