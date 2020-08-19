a708de7242ba320c941ee67aedbe1ec403d4cb55
DJMarcos <62577982+DJMarcos@users.noreply.github.com>
2020-08-19 13:55:26 -0700

    Add files via upload

---
+++ assiingment 100/assiingment 100/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace assiingment_100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            
+
+            Console.WriteLine("Have you ever had a DUI?");
+            bool AnswerTrue = true;
+            bool AnswerFalse = false;
+            Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Qualified?");
+            bool qualified = (age > 15 && AnswerFalse == false && tickets < 1);
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
