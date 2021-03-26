3afaad4d3297dc10042460aa9d75938ff7c117d4
DJMarcos <62577982+DJMarcos@users.noreply.github.com>
2021-03-26 00:39:45 -0700

    Add files via upload

---
+++ assingment 89/assingment 89/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace assingment_89
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+
+        }
+    }
+}
