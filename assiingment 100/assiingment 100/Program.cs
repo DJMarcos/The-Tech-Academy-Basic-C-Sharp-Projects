e031ac0803694563d8ff7aebfed95dddeee893bb
DJMarcos <62577982+DJMarcos@users.noreply.github.com>
2021-04-07 15:34:55 -0700

    Add files via upload

--- assiingment 100/assiingment 100/Program.cs
+++ assiingment 100/assiingment 100/Program.cs
@@ -6,18 +6,20 @@ namespace assiingment_100
     {
         static void Main(string[] args)
         {
+            //Asking the host a simple question with int
             Console.WriteLine("what is your age?");
             int age = Convert.ToInt32(Console.ReadLine());
             
-
+            
             Console.WriteLine("Have you ever had a DUI?");
-             bool DUI = Convert.ToBoolean(Console.ReadLine());
+            bool DUI = Convert.ToBoolean(Console.ReadLine());
 
             Console.WriteLine("How many speeding tickets do you have?");
             int tickets = Convert.ToInt32(Console.ReadLine());
 
-
+            // Final step adding all info gathered from host and using boolean logic
             Console.WriteLine("Qualified?");
+            // comparing the info gathered with the requirements
             bool qualified = (age > 15 && DUI == false && tickets < 50);
             Console.WriteLine(qualified);
             Console.ReadLine();
