45ace5d5e5adf6082f9862c82e2ac85ab2010ce1
DJMarcos <62577982+DJMarcos@users.noreply.github.com>
2021-04-09 05:31:02 -0700

    Add files via upload

--- assingment_229/assingment_229/Program.cs
+++ assingment_229/assingment_229/Program.cs
@@ -7,7 +7,7 @@ namespace assingment_229
         static void Main(string[] args)
         {
             // start by asking the host a simple bool question for age
-            Console.WriteLine("hellooo, lets see if you are elidigble, how old are you?");
+            Console.WriteLine("hellooo, lets see if you are eliigble, how old are you?");
             int answer = Convert.ToInt32(Console.ReadLine());
             bool Answer = answer == 21;
 
@@ -15,19 +15,19 @@ namespace assingment_229
             {
                 switch (answer)
                 {
-                    // adding different cases for the right age to be elidgible
+                    // adding different cases for the right age to be eligible
                     case 18:
-                        Console.WriteLine("you said your 18, your not elidgible.");
+                        Console.WriteLine("you said your 18, your not eligible.");
                         Console.WriteLine("how old are you?");
                         answer = Convert.ToInt32(Console.ReadLine());
                         break;
                     case 15:
-                        Console.WriteLine("you said your 15, your not elidgible.");
+                        Console.WriteLine("you said your 15, your not eligible.");
                         Console.WriteLine("how old are you?");
                         answer = Convert.ToInt32(Console.ReadLine());
                         break;
                     case 17:
-                        Console.WriteLine("you said your 17, your not elidgible.");
+                        Console.WriteLine("you said your 17, your not eligible.");
                         Console.WriteLine("how old are you?");
                         answer = Convert.ToInt32(Console.ReadLine());
                         break;
@@ -36,7 +36,7 @@ namespace assingment_229
                         Console.WriteLine("how old are you?");
                         answer = Convert.ToInt32(Console.ReadLine());
                         break;
-                    // default would be anything over 21 should be elidgible
+                    // default would be anything over 21 should be eligible
                     default:
                         Console.WriteLine("you are not old enough to work here.");
                         Console.WriteLine("how old are you?");
