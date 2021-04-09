74059ca9ee83fa192cbc24c3b490b04886d0f775
DJMarcos <62577982+DJMarcos@users.noreply.github.com>
2021-04-09 02:19:56 -0700

    Add files via upload

--- branching_assingnment/branching_assingnment/Program.cs
+++ branching_assingnment/branching_assingnment/Program.cs
@@ -11,6 +11,7 @@ namespace branching_assingnment
 
             // branching is needed to determine a price
             Console.WriteLine("Please enter the package weight");
+            // begin asking host for an answer
             int weight = Convert.ToInt32(Console.ReadLine());
             if (weight > 50)
             {
@@ -18,58 +19,31 @@ namespace branching_assingnment
             }
             else
             {
-                Console.WriteLine("Your package is valid");
+                // simple question inputs for host
+                Console.WriteLine("Please enter the package width");
+                int width = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter the package height");
+                int height = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter the package length");
+                int length = Convert.ToInt32(Console.ReadLine());
+                // multiply answers givin
+                int HWL = width * height * length;
+                //checking to see to talt amount is greater then 50
+                if (HWL > 50)
+                {
+                    Console.WriteLine("sorry, package is too large. Goodbye.");
+                }
+                else
+                {
+                    //last divide total amount
+                    int total = HWL * weight;
+                    int last = total / 100;
+                    Console.WriteLine("Your estimated total for shipping this package is : " + last);
+                    Console.ReadLine();
+                }
             }
-            Console.ReadLine();
-
-            //using branching again to check for price
-            Console.WriteLine("Please enter the package width");
-            int width = Convert.ToInt32(Console.ReadLine());
-
-            if (width > 50)
-            {
-                Console.WriteLine("Package is too big for Package Express");
-            }
-            else
-            {
-                Console.WriteLine("Your package checks out");
-            }
-            Console.ReadLine();
-
-            Console.WriteLine("Please enter the package height");
-            int height = Convert.ToInt32(Console.ReadLine());
-
-            if (height > 50)
-            {
-                Console.WriteLine("Package is too height for transport");
-            }
-            else
-            {
-                Console.WriteLine("Your package checks out");
-            }
-            Console.ReadLine();
-
-            Console.WriteLine("Please enter the package length");
-            int length = Convert.ToInt32(Console.ReadLine());
-            if (length > 50) 
-            {
-                Console.WriteLine("Package is too heavy yoo");
-            }
-            else
-            {
-                Console.WriteLine("Your package length checks out");
-            }
-            Console.ReadLine();
-
-            // as for the final step multiplying and dividing answers givin
-            int HWL = width * height * length;
-            int total = HWL * weight;
-            int last = total / 100;
-            Console.WriteLine("Your estimated total for shipping this package is : " + last);
-            Console.ReadLine();
-
-
-
         }
     }
 }
