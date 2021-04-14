4dbbc5d8a0199196bde7573f5afaa8e9879b4099
DJMarcos <62577982+DJMarcos@users.noreply.github.com>
2021-04-14 13:29:08 -0700

    Add files via upload

--- array_assingment/array_assingment/Program.cs
+++ array_assingment/array_assingment/Program.cs
@@ -7,16 +7,14 @@ namespace array_assingment
     {
         static void Main(string[] args)
         {
-            List<string> stringList = new List<string>();
-            stringList.Add("your boyyyy");
-            stringList.Add("heloooo");
-            stringList.Add("noooo");
+            // creating a one-dimensional array of strings
+            List<string> stringList = new List<string>() {"helloo", "no right now", "dose it work?" };
 
+            //asking the host to type an index
             Console.WriteLine("choose a number between 0 and 2.");
             int number = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(stringList[number]);
-            Console.ReadLine();
 
+            //using If Else to determain what index to show and stop program if a number is more then the index added
             if (number < 0 || number > 2)
             {
                 Console.WriteLine("the following index dose not exists");
@@ -25,17 +23,15 @@ namespace array_assingment
             {
                 Console.WriteLine("you chose: " + stringList[number]);
             }
-
-            List<int> intList = new List<int>();
-            intList.Add(10);
-            intList.Add(15);
-            intList.Add(25);
+            Console.ReadLine();
+            
+            //again creating a one-dimensional array of integers
+            List<int> intList = new List<int>() {10, 15, 25 };
 
             Console.WriteLine("choose a number between 0 and 2 to get an integer.");
             int number2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(intList[number2]);
-            Console.ReadLine();
 
+            //using the If Else to give out the info of the chosen index
             if (number2 < 0 || number2 > 2)
             {
                 Console.WriteLine("the following index dose not exists");
@@ -44,17 +40,17 @@ namespace array_assingment
             {
                 Console.WriteLine("you chose: " + intList[number2]);
             }
+            Console.ReadLine();
 
-            List<string[]> strList = new List<string[]>() {
-            new string[] {"h", "i", "i"},
-            new string[] {"lets", "goo"},
-            new string[] {"hope", "this", "works", "out"},
-            };
+            //creating a list of strings for user to select
+            List<string> strList = new List<string>(); 
+            strList.Add("hello");
+            strList.Add("lets");
+            strList.Add("hope this works out");
+            
 
             Console.WriteLine("select an index 0f 0 to 2 to display the content.");
             int number3 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(strList[number3]);
-            Console.ReadLine();
 
             if (number3 < 0 || number3 > 2)
             {
@@ -64,7 +60,7 @@ namespace array_assingment
             {
                 Console.WriteLine("you chose: " + strList[number3]);
             }
-
+            Console.ReadLine();
 
 
 
