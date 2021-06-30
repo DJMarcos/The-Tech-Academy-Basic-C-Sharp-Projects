0d06d75b16c5ea80cad3f26cadb15c8d2328deed
DJMarcos <62577982+DJMarcos@users.noreply.github.com>
2021-06-29 20:25:01 -0700

    Add files via upload

---
+++ calling_methods_assingment/calling_methods_assingment/MyMath.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calling_methods_assingment
+{
+    public class MyMath
+    {
+       public static int Math1(int answer)
+        {
+            return (answer + 2);
+        }
+        public static int Math2(int answer)
+        {
+            return (answer * 14);
+        }
+        public static int Math3(int answer)
+        {
+            return (answer / 2);
+        }
+    }
+}
---
+++ calling_methods_assingment/calling_methods_assingment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace calling_methods_assingment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what number do you want to do the math operation on?");
+            int answer = Convert.ToInt32(Console.ReadLine());
+            int yourA = MyMath.Math1(answer);
+            Console.WriteLine(yourA);
+            Console.ReadLine();
+
+
+        }
+
+    }
+}
