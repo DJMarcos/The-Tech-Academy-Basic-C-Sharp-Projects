8ed353bbf7ef0287387e6d41e1c9fa7a572c0b13
DJMarcos <62577982+DJMarcos@users.noreply.github.com>
2021-07-21 12:26:30 -0700

    Add files via upload

--- calling_methods_assingment/calling_methods_assingment/MyMath.cs
+++ calling_methods_assingment/calling_methods_assingment/MyMath.cs
@@ -2,21 +2,28 @@
 using System.Collections.Generic;
 using System.Text;
 
+//making methods that would make any sort of math operations
 namespace calling_methods_assingment
 {
+    // in order to have these methods worl. Must have a PUbliC class in order for the other main program to access it.
     public class MyMath
     {
+        //again in all methods in order to access it, must make it "public"
        public static int Math1(int answer)
         {
+            // with this code we are telling the method to return(display)
+            //the answer givin when asked a question and adding 2 at the end
             return (answer + 2);
         }
-        public static int Math2(int answer)
+        //Int Math2(int answer)
+        //telling the class that the method Math2 is going to be an int with an answer2 being an int as well
+        public static int Math2(int answer2)
         {
-            return (answer * 14);
+            return (answer2 * 14);
         }
-        public static int Math3(int answer)
+        public static int Math3(int answer3)
         {
-            return (answer / 2);
+            return (answer3 / 2);
         }
     }
 }
--- calling_methods_assingment/calling_methods_assingment/Program.cs
+++ calling_methods_assingment/calling_methods_assingment/Program.cs
@@ -6,13 +6,30 @@ namespace calling_methods_assingment
     {
         static void Main(string[] args)
         {
+            //asking the user a question and requireing them and reply with an answer
             Console.WriteLine("what number do you want to do the math operation on?");
+            //whatever they type in, its going to be saved as answer
             int answer = Convert.ToInt32(Console.ReadLine());
+            //this line of code will be whats going to display any answer after our method dose its thing
+            //without this line of code your method will not function and getting no results
             int yourA = MyMath.Math1(answer);
+            //doing a simple console.Readline to read whats going on
             Console.WriteLine(yourA);
             Console.ReadLine();
 
+            // from here on out, in order to display the rest of our methods.
+            //going to repeat the lines of code used to display the first method.
+            Console.WriteLine("What number would you like to do the math operation on?");
+            int answer2 = Convert.ToInt32(Console.ReadLine());
+            int YourAB = MyMath.Math2(answer2);
+            Console.WriteLine(YourAB);
+            Console.ReadLine();
 
+            Console.WriteLine("What number would you like to do the math opertion on, one last time lol");
+            int answer3 = Convert.ToInt32(Console.ReadLine());
+            int YourAC = MyMath.Math3(answer3);
+            Console.WriteLine(YourAC);
+            Console.ReadLine();
         }
 
     }
